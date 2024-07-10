2b5c45943dadeeabad9f8119ede54dc84f219c76
Kiper02 <permakovkirill@gmail.com>
2024-07-10 19:14:33 +0500

    Удаление .git в node_modules и обновление индекса

---
+++ client/src/longPulling.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import './App.css'
+import axios from 'axios'
+
+const LongPulling = () => {
+  const [messages, setMessages] = useState([])
+  const [value, setValue] = useState('')
+
+
+  useEffect(() => {
+    subscribe()
+  }, [])
+
+  const subscribe = async () => {
+    try {
+      const { data } = await axios.get('http://localhost:5000/get-messages');
+      setMessages(prev => [data, ...prev]);
+      await subscribe(); 
+    } catch (error) {
+      setTimeout(() => {
+        subscribe();
+      }, 500);
+    }
+  };
+  
+
+  const sendMessage = async() => {
+    console.log(value);
+    await axios.post('http://localhost:5000/new-messages', {
+      message: value,
+      id: Date.now()
+    }) 
+  }
+
+  return (
+    <div className='center'>
+      <div>
+        <div className='form'>
+          <input type='text' onChange={e => setValue(e.target.value)}/>
+          <button onClick={sendMessage}>Отправить</button>
+        </div>
+        <div className="messages">
+          {messages.map(mess => 
+            <div className='message' key={mess.id}>
+              {mess.message}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LongPulling;
