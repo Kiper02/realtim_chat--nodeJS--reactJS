61ab5a747107d75e0b9d266cc5681f0039ae2f88
Kiper02 <permakovkirill@gmail.com>
2024-07-10 19:25:18 +0500

    Event Sourcing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import LongPulling from './longPulling';
+import EventSourcing from './EventSourcing';
 
 const App = () => {
   return (
     <div>
-      <LongPulling />
+      {/* <LongPulling /> */}
+      <EventSourcing />
   </div>
   );
 };
